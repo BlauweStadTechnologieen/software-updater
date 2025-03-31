49ac87acc67603a16ed1aa426fdceabda2d1ae9d
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-03-31 13:50:03 +0100

    Renamed function

--- error_handler.py
+++ error_handler.py
@@ -1,6 +1,6 @@
 import freshdesk_ticket
 
-def handle_error(message:str, subject:str) -> bool:
-    print(f"{message} {subject}")
+def global_error_handler(subject:str, message:str) -> bool:
+    print(f"{subject},{message}")
     freshdesk_ticket.create_freshdesk_ticket(message, subject)
     return False
