8e7db582c38c92dfd64ed7e7673a9131a1b85a44
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-04-03 12:01:43 +0100

    Explicitly returned False within exception handling blocks

--- software_updater.py
+++ software_updater.py
@@ -28,15 +28,21 @@ def base_directory_validation() -> bool:
             raise FileNotFoundError(f"Base directory '{base_directory}' does not exist") 
         
     except KeyError as e:
+        
         global_error_handler(f"Base Directory Key Error","The key for the base directory env is either incorrect or missing. Please check for validation {e}")
+        
         return False
 
     except FileNotFoundError as e:
+        
         global_error_handler("Base Directory Existence", f"Unfortunately we unable to find the {base_directory} on the system {e}")
+        
         return False
 
     except Exception as e:
+        
         global_error_handler("Base Directory General Exception", f"{e}")
+        
         return False
     
     return True
@@ -52,18 +58,26 @@ def directory_validation(cwd:str) -> bool:
         directory_contents = os.listdir(cwd)
         
         if ".git" in directory_contents:
-
+            
             return True
         
         else:
            
-            return global_error_handler(f"{error_message_type}",f"This is not a local git repository. Please navigate to {cwd} and run 'git init' to initialise the folder.")
-    
+            global_error_handler(f"{error_message_type}",f"This is not a local git repository. Please navigate to {cwd} and run 'git init' to initialise the folder.")
+            
+            return False
+
     except FileNotFoundError as e:
-        return global_error_handler(f"{error_message_type} FileNotFound Error", f"FileNotFoundError {e} for Directory {cwd}")
+        
+        global_error_handler(f"{error_message_type} FileNotFound Error", f"FileNotFoundError {e} for Directory {cwd}")
+        
+        return False
 
     except Exception as e:
-        return global_error_handler(f"{error_message_type} Exception Error", f"Exception{e}")
+        
+        global_error_handler(f"{error_message_type} Exception Error", f"Exception{e}")
+        
+        return False
 
 def install_updates(cwd: str = None) -> bool:
     """Checks for new commits and dependancies in the remote repo by checking the status of the branch after fetching any new commits from the repo.
@@ -85,7 +99,7 @@ def install_updates(cwd: str = None) -> bool:
         if result.returncode != 0:
             
             global_error_handler(f"{error_message_type}", f"Git {cmd_name.capitalize()} Error # {result.returncode}.")
-
+            
             return False
 
         if cmd_name == "status" and "Your branch is behind" in result.stdout:
@@ -94,8 +108,8 @@ def install_updates(cwd: str = None) -> bool:
             
             if result.returncode != 0:
                 
-                global_error_handler(f"{error_message_type}", f"Git Pull Error {result.returncode} {result.stdout}")
-
+                global_error_handler(f"{error_message_type}", f"Git Pull Error {result.returncode}-{result.stdout}")
+                
                 return False
 
             print("New changes detected")
@@ -127,7 +141,7 @@ def check_for_updates():
 
         except Exception as e:
             
-            generate_support_ticket("Error in checking for updates", f"Unfortunately, there was an error in checking for updates. Please find the folloiwng error message {e}")
+            generate_support_ticket("Error in checking for updates", f"Unfortunately, there was an error in checking for updates. Please find the folloiwing error message {e}")
             
             return
 
