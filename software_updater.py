72e70f1518c2cff109fb36550d2a7b5c7d1157ae
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-04-03 11:22:17 +0100

    Added separate base_directory validation

--- software_updater.py
+++ software_updater.py
@@ -3,7 +3,7 @@ import os
 from install_new_dependencies import check_and_install_new_dependencies
 from dotenv import load_dotenv
 from pathlib import Path
-from error_handler import global_error_handler
+from error_handler import global_error_handler, generate_support_ticket
 import send_message as message
 
 load_dotenv()
@@ -12,22 +12,51 @@ BASE_DIRECTORY_ENV = {
     "BASE_DIRECTORY" : os.getenv("BASE_DIRECTORY")
 }
 
+base_directory = BASE_DIRECTORY_ENV["BASE_DIRECTORY"]
+
 def run_command(command: str, cwd: str = None):
     return subprocess.run(command, cwd=cwd, text=True, capture_output=True)
+
+def base_directory_validation() -> bool:
     
+    try:
+    
+        if not base_directory:
+            raise KeyError(f"The specified base directory of {base_directory} key is missing")
+
+        if not os.path.isdir(base_directory):
+            raise FileNotFoundError(f"Base directory '{base_directory}' does not exist") 
+        
+    except KeyError as e:
+        global_error_handler(f"Base Directory Key Error","The key for the base directory env is either incorrect or missing. Please check for validation {e}")
+        return False
+
+    except FileNotFoundError as e:
+        global_error_handler("Base Directory Existence", f"Unfortunately we unable to find the {base_directory} on the system {e}")
+        return False
+
+    except Exception as e:
+        global_error_handler("Base Directory General Exception", f"{e}")
+        return False
+    
+    return True
+
 def directory_validation(cwd:str) -> bool:
         
     error_message_type = "Directory Validation Error"
     
     try:
+
         os.chdir(cwd)
-        print("You should not see this message")
+        
         directory_contents = os.listdir(cwd)
         
         if ".git" in directory_contents:
+
             return True
+        
         else:
-            print(f"The {cwd} is unfortunatley not a git repo")
+           
             return global_error_handler(f"{error_message_type}",f"This is not a local git repository. Please navigate to {cwd} and run 'git init' to initialise the folder.")
     
     except FileNotFoundError as e:
@@ -42,10 +71,7 @@ def install_updates(cwd: str = None) -> bool:
     If there are any new dependacies, it will autmatically installs them"""
     
     error_message_type = "Update Installation Error"
-    
-    if not directory_validation(cwd):
-        return global_error_handler(f"{error_message_type}", f"The directory {cwd} is not valid. Please double-check the validity of the directory and try again. We apologise for the inconvenience caused. No, we are not Indian!")
-    
+        
     commands = {
         "fetch"  : ["git", "fetch"],
         "status" : ["git", "status"]
@@ -53,15 +79,24 @@ def install_updates(cwd: str = None) -> bool:
     }
     
     for cmd_name, cmd_args in commands.items():
-        result = run_command(cmd_args, cwd)
-        if result.returncode != 0:
-            return global_error_handler(f"{error_message_type}", f"Git {cmd_name.capitalize()} Error # {result.returncode}.")
         
+        result = run_command(cmd_args, cwd)
+        
+        if result.returncode != 0:
+            
+            global_error_handler(f"{error_message_type}", f"Git {cmd_name.capitalize()} Error # {result.returncode}.")
+
+            return False
+
         if cmd_name == "status" and "Your branch is behind" in result.stdout:
 
             result = run_command(["git","pull"], cwd)
+            
             if result.returncode != 0:
-                return global_error_handler(f"{error_message_type}", f"Git Pull Error {result.returncode} {result.stdout}")
+                
+                global_error_handler(f"{error_message_type}", f"Git Pull Error {result.returncode} {result.stdout}")
+
+                return False
 
             print("New changes detected")
             check_and_install_new_dependencies()
@@ -70,40 +105,31 @@ def install_updates(cwd: str = None) -> bool:
 
 def check_for_updates():
 
-    try:
-        
-        base_directory              = BASE_DIRECTORY_ENV["BASE_DIRECTORY"]
-        
-        if not base_directory:
-            raise KeyError("BASE_DIRECTORY key not found in BASE_DIRECTORY_ENV.")
+    updated_software_packages = []
 
-        if not os.path.isdir(base_directory):
-            raise FileNotFoundError(f"Base directory '{base_directory}' does not exist") 
+    if base_directory_validation():
         
-        updated_software_packages   = []
-    
-        for software_package in os.listdir(base_directory):
-                    
-            cwd = os.path.join(base_directory, software_package)
+        try:
             
-            if not install_updates(cwd):
-                continue
+            for software_package in os.listdir(base_directory):
+                
+                cwd = os.path.join(base_directory, software_package)
+                
+                if not install_updates(cwd):
+                                        
+                    continue 
 
-            updated_software_packages.append(software_package)
-        
-        message.send_message(updated_software_packages)
+                updated_software_packages.append(software_package)
+            
+            if updated_software_packages:
 
-    except KeyError as e:
+                message.send_message(updated_software_packages)
 
-        return global_error_handler("Key Error", f"{e}")
-    
-    except FileNotFoundError as e:
-
-        return global_error_handler("File Not Found Error", f"{e}")
-    
-    except Exception as e:
-
-        return global_error_handler("Error","f{e}")
+        except Exception as e:
+            
+            generate_support_ticket("Error in checking for updates", f"Unfortunately, there was an error in checking for updates. Please find the folloiwng error message {e}")
+            
+            return
 
 if __name__ == "__main__":
     check_for_updates()
