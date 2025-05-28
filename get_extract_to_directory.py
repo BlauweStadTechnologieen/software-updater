b3c98c2683b719173064f1cfa9c5f65bff725ae7
Blue City Capital Technologies, Inc <engineering@bluecitycapital.com>
2025-05-28 16:25:57 +0100

    Initial

---
+++ get_extract_to_directory.py
@@ -0,0 +1,37 @@
+from extract_to import EXTRACT_TO
+import os
+from error_handler import global_error_handler
+
+def get_extract_to_directory() -> str:
+    """
+    Retrieves the base directory from the environment variable.
+    Returns:
+        str: The path of the base directory.
+    Exceptions:
+        KeyError: Raised when the EXTRACT_TO environment variable is not set.
+        FileNotFoundError: Raised when the specified EXTRACT_TO directory does not exist.
+    """
+    
+    try:
+        
+        if not EXTRACT_TO:
+            
+            raise KeyError("EXTRACT_TO environment variable is not set.")
+        
+        if not os.path.exists(EXTRACT_TO):
+            
+            raise FileNotFoundError(f"The specified EXTRACT_TO directory '{EXTRACT_TO}' does not exist.")
+                
+        return EXTRACT_TO
+    
+    except KeyError as e:
+        
+        global_error_handler("Missing EXTRACT_TO environment variable", f"Please ensure that the EXTRACT_TO variable is set in your .env file. {e}")
+        
+        return None
+    
+    except FileNotFoundError as e:
+        
+        global_error_handler("Invalid EXTRACT_TO directory", f"The specified EXTRACT_TO directory '{EXTRACT_TO}' does not exist. Please check the path. {e}")
+        
+        return None
